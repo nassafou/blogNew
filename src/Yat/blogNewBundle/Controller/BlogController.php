743a1dd0125fb70b593e51ca823103ce44b625f8
nassafou <ntyoussouf@gmail.com>
2016-04-18 12:32:42 -0700

    modif

--- src/Yat/blogNewBundle/Controller/BlogController.php
+++ src/Yat/blogNewBundle/Controller/BlogController.php
@@ -7,37 +7,64 @@ use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
+       // on ne sait pas combien de page il y a
+       // Mais on sait qu'une page doit être supérieur ou égale à 1
+       if($page < 1)
+       {
+        // on déclenche une exception NotFoundHttpException
+        // cela  va affichier la page d'erreur 404 (a personaliser plus tard)
+        throw $this->createNotFoundException('Page inexistante (page = '.$page.')');
+       }
+    
+    // Ici, on recupper la liste des articles 
        
-       $id_article = 5;
        
-       //$url = $this->generateUrl('yatblog_voir', array('id' => $id));
-       
-       //return $this->redirect($url);
-       
-       // return new Response("Hello World !");
-        //return $this->render('BlogNewBundle:Default:index.html.twig', array('name' => $name));
-        return $this->render('BlogNewBundle:Blog:index.html.twig', array('id_article' => $id_article));
+        return $this->render('BlogNewBundle:Blog:index.html.twig');
     }
     public function voirAction($id)
     {
-       return new Response("Voir l'article ".$id."." );
-        //return $this->render('BlogNewBundle:Default:index.html.twig', array('name' => $name));
-        //return $this->render('BlogNewBundle:Blog:index.html.twig', array('name' => 'youf'));
+       // Ici, on recupere l'article correspondant à l'id $id 
+      return $this->render('BlogNewBundle:Blog:voir.html.twig', array('id' => $id ));
+        
     }
     
     public function voirSlugAction($slug,$annee,$format)
     {
        return new Response("Voir l'article correspondant au slug , ".$slug." l' annnée ".$annee." au format.".$format."." );
-        //return $this->render('BlogNewBundle:Default:index.html.twig', array('name' => $name));
-        //return $this->render('BlogNewBundle:Blog:index.html.twig', array('name' => 'youf'));
+        
     }
     
+    // ajouter cette methode a ajouterAction
      public function ajouterAction()
     {
-       
-        return $this->render('BlogNewBundle:Blog:index.html.twig', array('name' => 'youf'));
+        // La gestion de formulaire
+        if($this->get('request')->getMethod() == 'POST')
+        {
+            // Ici, on s'occupera de la création et de la gestion du formulaire
+            $this->get('session')->getFlashBag()->add('notice', 'Article bien enregistré');
+            // Puis on redirige vers la page de visualisation
+            return $this->redirect($this->generateUrl('yatblog_voir', array('id' => 5)));
+        }
+        
+        // Si on n'est pas en POST , alors on affiche le formulaire
+        return $this->render('BlogNewBundle:Blog:ajouter.html.twig');    
+    }
+    public function modifierAction($id)
+    {
+        //Ici On recupere l'article  correspondant à $id
+        
+        //Ici, on s'occupe de la gestion et de la création  du formulaire
+        
+        return $this->render('BlogNewBundle:Blog:modifier.html.twig');
+    }
+    
+    public function supprimerAction($id)
+    {
+        //Ici On recupere l'article  correspondant à $id
+        // Ici, On gere la suppression de l'article
+        return $this->render('BlogNewBundle:Blog:supprimer.html.twig'); 
     }
     
 }
