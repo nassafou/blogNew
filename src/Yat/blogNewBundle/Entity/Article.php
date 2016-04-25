a4d24ba94cc3081a304fba66b8fba77b2bd431cb
nassafou <ntyoussouf@gmail.com>
2016-04-25 10:59:54 -0700

    entity

--- src/Yat/blogNewBundle/Entity/Article.php
+++ src/Yat/blogNewBundle/Entity/Article.php
@@ -1,58 +1,221 @@
 <?php
+
 namespace Yat\blogNewBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Yat\blogNewBundle\Entity\ArticleRepository")
+ */
 class Article
 {
-    protected $id;
-    protected $date;
-    protected $titre;
-    protected $auteur;
-    protected $contenu;
-    
-    // getters/setters
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="auteur", type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="publication", type="boolean")
+     */
+    private $publication;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenu", type="text")
+     */
+    private $contenu;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateEdition", type="date")
+     */
+    private $dateEdition;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
     public function getId()
     {
         return $this->id;
-        
     }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Article
+     */
     public function setDate($date)
     {
         $this->date = $date;
+
+        return $this;
     }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
     public function getDate()
     {
         return $this->date;
-        
     }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Article
+     */
     public function setTitre($titre)
     {
         $this->titre = $titre;
+
+        return $this;
     }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
     public function getTitre()
     {
         return $this->titre;
-        
     }
+
+    /**
+     * Set auteur
+     *
+     * @param string $auteur
+     *
+     * @return Article
+     */
     public function setAuteur($auteur)
     {
         $this->auteur = $auteur;
+
+        return $this;
     }
-    public function getActeur()
+
+    /**
+     * Get auteur
+     *
+     * @return string
+     */
+    public function getAuteur()
     {
-        return $this->auteur;  
+        return $this->auteur;
     }
+
+    /**
+     * Set publication
+     *
+     * @param boolean $publication
+     *
+     * @return Article
+     */
+    public function setPublication($publication)
+    {
+        $this->publication = $publication;
+
+        return $this;
+    }
+
+    /**
+     * Get publication
+     *
+     * @return boolean
+     */
+    public function getPublication()
+    {
+        return $this->publication;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     *
+     * @return Article
+     */
     public function setContenu($contenu)
     {
         $this->contenu = $contenu;
+
+        return $this;
     }
+
+    /**
+     * Get contenu
+     *
+     * @return string
+     */
     public function getContenu()
     {
-        return $this->contenu;  
+        return $this->contenu;
     }
-    
-    
+
+    /**
+     * Set dateEdition
+     *
+     * @param \DateTime $dateEdition
+     *
+     * @return Article
+     */
+    public function setDateEdition($dateEdition)
+    {
+        $this->dateEdition = $dateEdition;
+
+        return $this;
+    }
+
+    /**
+     * Get dateEdition
+     *
+     * @return \DateTime
+     */
+    public function getDateEdition()
+    {
+        return $this->dateEdition;
+    }
 }
+
